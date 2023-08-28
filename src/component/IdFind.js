50c0e4dbe775efc1d998b59152bb49453e679955
DESKTOP-JUSC5N8\Administrator <builddav2009@gmail.com>
2023-08-28 21:03:16 +0900

    충돌작업테스트

--- src/component/IdFind.js
+++ src/component/IdFind.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const IdFind = () => {
-  return <div>아이디파인드asdasds</div>;
+  return (
+    <div>
+      아이디파인드asdasds
+      <div>adnsladfbhnasfn'ls</div>
+    </div>
+  );
 };
 
 export default IdFind;
