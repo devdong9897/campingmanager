097779a2d42aff1c2b3e461cba21ed8634a4233c
devdong9897 <131754770+devdong9897@users.noreply.github.com>
2023-09-08 21:56:11 +0900

    feat: 게시판 작업중

--- src/api/adminboardFetch.js
+++ src/api/adminboardFetch.js
@@ -1,3 +1,24 @@
-import React from 'react'
+import axios from "axios";
 
-const get
+const postAddCategory = async () => {
+  try {
+    const res = await axios.post("/api/community/insCategory");
+    const result = res.data;
+    console.log("postAddCategory 완료", result);
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// const postBasket = async iitem => {
+//   try {
+//     const res = await axios.post(`/api/cart`, {
+//       iitem: iitem,
+//       quantity: 1,
+//     });
+//     console.log("postbasket 실행완료");
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
